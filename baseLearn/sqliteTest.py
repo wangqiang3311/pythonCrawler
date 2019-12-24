acaff36bcb28d86f23966d58fa3fb4debf1966eb
wangqiang3311 <wangqiang3311@163.com>
2019-12-24 19:02:52 +0800

    change file name

--- baseLearn/sqliteTest.py
+++ baseLearn/sqliteTest.py
@@ -2,7 +2,7 @@ import sqlite3
 con=sqlite3.connect('d:/test.db')
 cur=con.cursor()
 #cur.execute('create table person(id integer primary key,name varchar(20),age integer)')
-cur.execute(' insert into person Values(?,?,?)',(1,'wbq',20))
+cur.execute(' insert into person Values(?,?,?)',(2,'wbq',20))
 con.commit()
 cur.execute('select * from person')
 res=cur.fetchall()
